f2ab0254eff7acec770a3067f84137394b67a46a
Lozya <40338818+Lozya@users.noreply.github.com>
2018-07-27 15:29:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,55 @@
-$('.burgerMenu').on('click',function () {
-    $('.burgerMenu__burgerMenuItems').toggleClass('burgerMenu__burgerMenuItems-active');
-});
+window.onload = function () {
+    var burgerMenu = document.querySelector('.burgerMenu');
+    var burgerMenuItems = document.querySelector('.burgerMenu__burgerMenuItems');
+    var leftSlider = document.querySelector('#left');
+    var rightSlider = document.querySelector('#right');
+    var line = document.querySelector('.partnersSection__slider');
+    var left = 0;
+
+    burgerMenu.onclick = function(){
+        burgerMenuItems.classList.toggle('burgerMenu__burgerMenuItems-active');
+    };
+
+    leftSlider.onclick = function()
+    {
+        if(window.matchMedia('all and (max-width: 700px)').matches) {
+            left = left - 267;
+
+            if (left < -534) {
+                left = 0;
+            }
+            line.style.left = left + 'px';
+        } else {
+            if (window.matchMedia('all and (max-width: 1024px)').matches) {
+                left = left - 267;
+
+                if (left <= -534) {
+                    left = 0;
+                }
+                line.style.left = left + 'px';
+            }
+        }
+    };
+    rightSlider.onclick = function()
+    {
+        if(window.matchMedia('all and (max-width: 700px)').matches) {
+            left = left + 267;
+
+            if (left >= 267) {
+                left = -534;
+            }
+            line.style.left = left + 'px';
+        }else {
+            if (window.matchMedia('all and (max-width: 1024px)').matches) {
+                left = left + 267;
+
+                if (left >= 267) {
+                    left = -267;
+                }
+                line.style.left = left + 'px';
+            }
+        }
+    };
+};
+
+
